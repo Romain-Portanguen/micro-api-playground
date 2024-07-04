88c5d746b6a450006a0280244569434e9f8b1cd8
Romain Portanguen <portanguenr@gmail.com>
2024-07-04 11:23:40 +0200

    Managing Jest configuration

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const moduleFileExtensions = ['ts', 'js', 'json', 'node'];
-export const roots = ['<rootDir>/src'];
-export const testMatch = ['**/?(*.)+(spec|test).[tj]s?(x)'];
